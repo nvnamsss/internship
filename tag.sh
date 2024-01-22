0da986b6661e53492a636e4ed2a5a29b441f59b6
nvnamsss <nvnam.c@gmail.com>
2024-01-22 15:31:26 +0700

    removed token

--- tag.sh
+++ tag.sh
@@ -1,4 +1,3 @@
-# doctl registry login -t dop_v1_534dbe2abe4b33d28cdc765963a7cde58fbd3143f5e62dbd0847fbe6048df42e
 
 docker tag internship-app registry.digitalocean.com/internship/internship-app:0.0.1
 docker push registry.digitalocean.com/internship/internship-app:0.0.1
